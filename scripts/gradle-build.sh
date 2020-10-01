ddd2135a71d88d32d554936c7e564396ad93773e
Omar Yusuf <omar.yusuf@cardinalhealth.com>
2020-10-01 14:02:50 -0400

    Update gradle-build.sh

--- scripts/gradle-build.sh
+++ scripts/gradle-build.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+set -e -x # fail fast
+export TERM=xterm-color
+# ensure that Gradle uses root location for caching
+export ROOT_FOLDER=$( pwd )
+export GRADLE_USER_HOME="${ROOT_FOLDER}/project-source/.gradle"
+echo "GRADLE_USER_HOME: ${GRADLE_USER_HOME}"
+resource_dir=$(dirname $0)
+
+echo ${resource_dir}
+echo "current environemnt : "$CURRENT_ENV
+mkdir sonarqube-analysis-input/build
+mkdir build-out/version
+# mkdir sonarqube-analysis-input/build/jacoco
+mkdir project-source/server/build
+mkdir project-source/server/build/libs
 cd project-source
-chmod +x ./gradlew
+
+#build 
+chmod 755 gradlew
 ./gradlew clean build
